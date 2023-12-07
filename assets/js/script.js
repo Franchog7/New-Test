cb33424b2113fce531b33f204739e9157b86265a
Frank <francho_714@hotmail.com>
2023-12-07 18:04:34 -0400

    JS-created

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+let typeNumber = 45;
+let typeString = "45";
+let typeboolean = false;
+let typeundefined = undefined;
+let tpyenull = null;
+
+
+let typelist = [
+    50,
+    77,
+    29,
+    100,
+    47,
+    "Leonardo",
+    "Brandon",
+    "Jose",
+    "miguel",
+    "Francisco",
+    false
+]
+
+let typeObject = {
+    name: "Frank",
+    email: "francho714@gmail.com",
+    pass: "1234"
+}
+
+console.log(typeObject["name"])
